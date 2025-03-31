b26c2646b2f2a47d005bb58ff063cf6c42c4ff36
BenH9999 <ben.houghton7070@outlook.com>
2025-04-01 00:27:05 +0100

    annoying errors

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -4,15 +4,15 @@ import (
 	"net/http"
 	"fmt"
 
-	"github.com/BenH9999/DevCollab/backend/internal/handlers"
+	_"github.com/BenH9999/DevCollab/backend/internal/handlers"
 )
 
 func SetupRouter() http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/api/login", handlers.Login)
-	mux.HandleFunc("/api/register", handlers.Register)
-	mux.HandleFunc("/api/logout", handlers.Logout)
+	//mux.HandleFunc("/api/login", handlers.Login)
+	//mux.HandleFunc("/api/register", handlers.Register)
+	//mux.HandleFunc("/api/logout", handlers.Logout)
 
 	fmt.Println("Routes setup successfully")
 	
